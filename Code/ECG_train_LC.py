e9b1418d96222f743850f0d8ecd3ea9801ad91c9
QuYue <quyue1541@mail.dlut.edu.cn>
2019-05-07 17:24:06 +0800

    add Link Constraits

--- Code/ECG_train_LC.py
+++ Code/ECG_train_LC.py
@@ -78,7 +78,7 @@ if __name__ == '__main__':
                 x, y = x.cuda(), y.cuda()
             diagnosis.train()  # train model
             output, embedding = diagnosis(x)
-            link_constraints = model_LC.LinkConstraints(embedding, y, weight_decay=Args)
+            link_constraints = model_LC.LinkConstraints(embedding, y, weight_decay=Args.LCw)
             loss = loss_func(output, y)  # loss
             loss += link_constraints
             optimizer.zero_grad()  # clear gradients for next train
